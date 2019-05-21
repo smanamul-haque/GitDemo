8cfc38a40acfc2be0003f9266f80c19a8900ee6f
Anamul Haque <smanam.haque@gmail.com>
2019-05-21 02:52:24 -0400

    added 3 more line from home machine

--- specs/test1.js
+++ specs/test1.js
@@ -1,3 +1,6 @@
 console.log("First Test Cases");
 console.log("2nd Test Cases");
 console.log("third Test Cases");
+console.log("First Test Cases  1");
+console.log("2nd Test Cases 2");
+console.log("third Test Cases   3");
